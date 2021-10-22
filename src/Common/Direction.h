a50bdf72111db39313a3d5462df3212b0fb8fce1
Alexandre Spatola <alexandre.spatola@etu.umontpellier.fr>
2021-10-22 17:25:34 +0200

    Add basic types

---
+++ src/Common/Direction.h
@@ -0,0 +1,16 @@
+#ifndef BOAT_DIRECTION_H
+#define BOAT_DIRECTION_H
+
+enum class Direction
+{
+	North,
+	NorthEast,
+	East,
+	SouthEast,
+	South,
+	SouthWest,
+	West,
+	NorthWest
+};
+
+#endif
---
+++ src/Common/Point.h
@@ -0,0 +1,10 @@
+#ifndef BOAT_POINT_H
+#define BOAT_POINT_H
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+#endif
